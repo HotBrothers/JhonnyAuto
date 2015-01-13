5c734ec077fcaa3dee58a410bc139e0d00f931e4
karaopea <karaopea@gmail.com>
2015-01-13 18:14:56 +0900

    이미지 스크린샷 진행중
    전체화면 중 크롭해서 저장해야됨

--- JhonnyAutoCore.cpp
+++ JhonnyAutoCore.cpp
@@ -5,6 +5,28 @@
 
 using namespace cv;
 
+// helper function prototypes
+static BITMAPINFO *PrepareRGBBitmapInfo(WORD wWidth, 
+                                        WORD wHeight);
+ 
+static void ShrinkDataInt(BYTE *pInBuff, 
+                          WORD wWidth, 
+                          WORD wHeight,
+                          BYTE *pOutBuff, 
+                          WORD wNewWidth, 
+                          WORD wNewHeight);
+ 
+static void EnlargeDataInt(BYTE *pInBuff, 
+                           WORD wWidth, 
+                           WORD wHeight,
+                           BYTE *pOutBuff, 
+                           WORD wNewWidth, 
+                           WORD wNewHeight);
+
+HBITMAP ScaleBitmapInt(HBITMAP hBmp, 
+                       WORD wNewWidth, 
+                       WORD wNewHeight);
+
 
 
 JhonnyAutoCore::JhonnyAutoCore()
@@ -282,35 +304,70 @@ bool JhonnyAutoCore::captureScreen(HBITMAP* hBitmap, RECT rect)
 
 bool JhonnyAutoCore::captureScreen(HWND hTargetWnd, RECT rect, HBITMAP* returnBitmap)
 {
+	
     CRect rct;
     if(hTargetWnd)
         ::GetWindowRect(hTargetWnd, &rct);
     else
         return FALSE;
+		
+	if(returnBitmap != NULL)
+		DeleteObject(returnBitmap);
 
-    HBITMAP hBitmap = NULL;
     HBITMAP hOldBitmap = NULL;
     BOOL bSuccess = FALSE;
 
     HDC hDC = ::GetDC(hTargetWnd);
+
+	
+	// DIB의 형식을 정의한다.
+	BITMAPINFO dib_define;
+	dib_define.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	dib_define.bmiHeader.biWidth = rct.right - rct.left;
+	dib_define.bmiHeader.biHeight = rct.bottom - rct.top;
+	dib_define.bmiHeader.biPlanes = 1;
+	dib_define.bmiHeader.biBitCount = 24;
+	dib_define.bmiHeader.biCompression = BI_RGB;
+	dib_define.bmiHeader.biSizeImage = ((((rct.right - rct.left) * 24 + 31) & ~31) >> 3) * (rct.bottom - rct.top);
+	dib_define.bmiHeader.biXPelsPerMeter = 0;
+	dib_define.bmiHeader.biYPelsPerMeter = 0;
+	dib_define.bmiHeader.biClrImportant = 0;
+	dib_define.bmiHeader.biClrUsed = 0;
+
+	// DIB의 내부 이미지 비트 패턴을 참조할 포인터 변수
+	BYTE *p_image_data = NULL;
+
+	// dib_define에 정의된 내용으로 DIB를 생성한다.
+	HBITMAP hBitmap = ::CreateDIBSection(hDC, &dib_define, DIB_RGB_COLORS, (void **)&p_image_data, 0, 0);
+
+
     HDC hMemDC = ::CreateCompatibleDC(hDC);
-    hBitmap = ::CreateCompatibleBitmap(hDC, rct.Width(), rct.Height());
+
+	
 
     if(!hBitmap)
         return FALSE;
 
     hOldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
 
-    if(!::PrintWindow(hTargetWnd, hMemDC, PW_CLIENTONLY))
+    if(!::PrintWindow(hTargetWnd, hMemDC, 0))
         bSuccess = FALSE;
     else
         bSuccess = TRUE;
 
-	::BitBlt(hMemDC, rect.left, rect.top, (rect.right - rect.left), (rect.bottom - rect.top), hDC, 0, 0, SRCCOPY);
+	//::BitBlt(hMemDC, rect.left, rect.top, rect.right , rect.bottom, hDC, 0, 0, SRCCOPY);
+	//::BitBlt(hMemDC, 0, 0, rect.right - rect.left , rect.bottom - rect.top, hDC, -rect.left, -rect.top, SRCCOPY);
+	//::BitBlt(h_memory_dc, -startX, -startY, SEARCH_RECT_WIDTH+startX, SEARCH_RECT_HEGIHT+startY, h_screen_dc, 0, 0, SRCCOPY);
 
-	*returnBitmap = hBitmap;
+	::BitBlt(hMemDC, 0, 0, rect.right - rect.left , rect.bottom - rect.top, hMemDC, -rect.left, -rect.top, SRCCOPY);
+	 //hBitmap = ::CreateDiscardableBitmap
+	
+	//*returnBitmap = ScaleBitmapInt(hBitmap, rect.right - rect.left,   rect.bottom - rect.top);
 
-    SelectObject(hMemDC, hOldBitmap);
+	//::SelectObject(hMemDC, hOldBitmap);
+	
+	
+   
     //DeleteObject(hBitmap);
     ::DeleteDC(hMemDC);
     ::ReleaseDC(hTargetWnd, hDC);
@@ -507,3 +564,316 @@ bool JhonnyAutoCore::matching(IplImage* background, IplImage* object, CvPoint* r
   
 }
 
+
+
+
+HBITMAP ScaleBitmapInt(HBITMAP hBmp, 
+					   WORD wNewWidth, 
+					   WORD wNewHeight)
+{
+	BITMAP bmp;
+	::GetObject(hBmp, sizeof(BITMAP), &bmp);
+
+	// check for valid size
+	if((bmp.bmWidth > wNewWidth 
+		&& bmp.bmHeight < wNewHeight) 
+		|| (bmp.bmWidth < wNewWidth 
+		&& bmp.bmHeight > wNewHeight))
+		return NULL;
+
+	HDC hDC = ::GetDC(NULL);
+	BITMAPINFO *pbi = PrepareRGBBitmapInfo((WORD)bmp.bmWidth, 
+		(WORD)bmp.bmHeight);
+	BYTE *pData = new BYTE[pbi->bmiHeader.biSizeImage];
+
+	::GetDIBits(hDC, hBmp, 0, bmp.bmHeight, pData, pbi, DIB_RGB_COLORS);
+
+	delete pbi;
+	pbi = PrepareRGBBitmapInfo(wNewWidth, wNewHeight);
+	BYTE *pData2 = new BYTE[pbi->bmiHeader.biSizeImage];
+
+	if(bmp.bmWidth >= wNewWidth 
+		&& bmp.bmHeight >= wNewHeight)
+		ShrinkDataInt(pData, 
+		(WORD)bmp.bmWidth, 
+		(WORD)bmp.bmHeight,
+		pData2, 
+		wNewWidth, 
+		wNewHeight);
+	else
+		EnlargeDataInt(pData, 
+		(WORD)bmp.bmWidth, 
+		(WORD)bmp.bmHeight,
+		pData2, 
+		wNewWidth, 
+		wNewHeight);
+
+	delete pData;
+
+	HBITMAP hResBmp = ::CreateCompatibleBitmap(hDC, 
+		wNewWidth, 
+		wNewHeight);
+
+	::SetDIBits(hDC, 
+		hResBmp, 
+		0, 
+		wNewHeight, 
+		pData2, 
+		pbi, 
+		DIB_RGB_COLORS);
+
+	::ReleaseDC(NULL, hDC);
+
+	delete pbi;
+	delete pData2;
+
+	return hResBmp;
+}
+
+///////////////////////////////////////////////////////////
+
+BITMAPINFO *PrepareRGBBitmapInfo(WORD wWidth, WORD wHeight)
+{
+	BITMAPINFO *pRes = new BITMAPINFO;
+	::ZeroMemory(pRes, sizeof(BITMAPINFO));
+	pRes->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	pRes->bmiHeader.biWidth = wWidth;
+	pRes->bmiHeader.biHeight = wHeight;
+	pRes->bmiHeader.biPlanes = 1;
+	pRes->bmiHeader.biBitCount = 24;
+
+	pRes->bmiHeader.biSizeImage = 
+		((3 * wWidth + 3) & ~3) * wHeight;
+
+	return pRes;
+}
+
+///////////////////////////////////////////////////////////
+
+static int *CreateCoeffInt(int nLen, int nNewLen, BOOL bShrink)
+{
+	int nSum = 0, nSum2;
+	int *pRes = new int[2 * nLen];
+	int *pCoeff = pRes;
+	int nNorm = (bShrink) 
+		? (nNewLen << 12) / nLen : 0x1000;
+	int	nDenom = (bShrink)? nLen : nNewLen;
+
+	::ZeroMemory(pRes, 2 * nLen * sizeof(int));
+	for(int i = 0; i < nLen; i++, pCoeff += 2)
+	{
+		nSum2 = nSum + nNewLen;
+		if(nSum2 > nLen)
+		{
+			*pCoeff = ((nLen - nSum) << 12) / nDenom;
+			pCoeff[1] = ((nSum2 - nLen) << 12) / nDenom;
+			nSum2 -= nLen;
+		}
+		else
+		{
+			*pCoeff = nNorm;
+			if(nSum2 == nLen)
+			{
+				pCoeff[1] = -1;
+				nSum2 = 0;
+			}
+		}
+		nSum = nSum2;
+	}
+
+	return pRes;
+}
+
+///////////////////////////////////////////////////////////
+
+void ShrinkDataInt(BYTE *pInBuff, 
+				   WORD wWidth, 
+				   WORD wHeight,
+				   BYTE *pOutBuff, 
+				   WORD wNewWidth, 
+				   WORD wNewHeight)
+{
+	BYTE  *pLine = pInBuff, *pPix;
+	BYTE  *pOutLine = pOutBuff;
+	DWORD dwInLn = (3 * wWidth + 3) & ~3;
+	DWORD dwOutLn = (3 * wNewWidth + 3) & ~3;
+	int   x, y, i, ii;
+	BOOL  bCrossRow, bCrossCol;
+	int   *pRowCoeff = CreateCoeffInt(wWidth, 
+		wNewWidth, 
+		TRUE);
+	int   *pColCoeff = CreateCoeffInt(wHeight, 
+		wNewHeight, 
+		TRUE);
+	int   *pXCoeff, *pYCoeff = pColCoeff;
+	DWORD dwBuffLn = 3 * wNewWidth * sizeof(DWORD);
+	DWORD *pdwBuff = new DWORD[6 * wNewWidth];
+	DWORD *pdwCurrLn = pdwBuff, 
+		*pdwCurrPix, 
+		*pdwNextLn = pdwBuff + 3 * wNewWidth;
+	DWORD dwTmp, *pdwNextPix;
+
+	::ZeroMemory(pdwBuff, 2 * dwBuffLn);
+
+	y = 0;
+	while(y < wNewHeight)
+	{
+		pPix = pLine;
+		pLine += dwInLn;
+
+		pdwCurrPix = pdwCurrLn;
+		pdwNextPix = pdwNextLn;
+
+		x = 0;
+		pXCoeff = pRowCoeff;
+		bCrossRow = pYCoeff[1] > 0;
+		while(x < wNewWidth)
+		{
+			dwTmp = *pXCoeff * *pYCoeff;
+			for(i = 0; i < 3; i++)
+				pdwCurrPix[i] += dwTmp * pPix[i];
+			bCrossCol = pXCoeff[1] > 0;
+			if(bCrossCol)
+			{
+				dwTmp = pXCoeff[1] * *pYCoeff;
+				for(i = 0, ii = 3; i < 3; i++, ii++)
+					pdwCurrPix[ii] += dwTmp * pPix[i];
+			}
+			if(bCrossRow)
+			{
+				dwTmp = *pXCoeff * pYCoeff[1];
+				for(i = 0; i < 3; i++)
+					pdwNextPix[i] += dwTmp * pPix[i];
+				if(bCrossCol)
+				{
+					dwTmp = pXCoeff[1] * pYCoeff[1];
+					for(i = 0, ii = 3; i < 3; i++, ii++)
+						pdwNextPix[ii] += dwTmp * pPix[i];
+				}
+			}
+			if(pXCoeff[1])
+			{
+				x++;
+				pdwCurrPix += 3;
+				pdwNextPix += 3;
+			}
+			pXCoeff += 2;
+			pPix += 3;
+		}
+		if(pYCoeff[1])
+		{
+			// set result line
+			pdwCurrPix = pdwCurrLn;
+			pPix = pOutLine;
+			for(i = 3 * wNewWidth; i > 0; i--, pdwCurrPix++, pPix++)
+				*pPix = ((LPBYTE)pdwCurrPix)[3];
+
+			// prepare line buffers
+			pdwCurrPix = pdwNextLn;
+			pdwNextLn = pdwCurrLn;
+			pdwCurrLn = pdwCurrPix;
+			::ZeroMemory(pdwNextLn, dwBuffLn);
+
+			y++;
+			pOutLine += dwOutLn;
+		}
+		pYCoeff += 2;
+	}
+
+	delete [] pRowCoeff;
+	delete [] pColCoeff;
+	delete [] pdwBuff;
+} 
+
+///////////////////////////////////////////////////////////
+
+void EnlargeDataInt(BYTE *pInBuff, 
+					WORD wWidth, 
+					WORD wHeight,
+					BYTE *pOutBuff, 
+					WORD wNewWidth, 
+					WORD wNewHeight)
+{
+	BYTE  *pLine = pInBuff, 
+		*pPix = pLine, 
+		*pPixOld, 
+		*pUpPix, 
+		*pUpPixOld;
+	BYTE  *pOutLine = pOutBuff, *pOutPix;
+	DWORD dwInLn = (3 * wWidth + 3) & ~3;
+	DWORD dwOutLn = (3 * wNewWidth + 3) & ~3;
+	int   x, y, i;
+	BOOL  bCrossRow, bCrossCol;
+	int   *pRowCoeff = CreateCoeffInt(wNewWidth, 
+		wWidth, 
+		FALSE);
+	int   *pColCoeff = CreateCoeffInt(wNewHeight, 
+		wHeight, 
+		FALSE);
+	int   *pXCoeff, *pYCoeff = pColCoeff;
+	DWORD dwTmp, dwPtTmp[3];
+
+	y = 0;
+	while(y < wHeight)
+	{
+		bCrossRow = pYCoeff[1] > 0;
+		x = 0;
+		pXCoeff = pRowCoeff;
+		pOutPix = pOutLine;
+		pOutLine += dwOutLn;
+		pUpPix = pLine;
+		if(pYCoeff[1])
+		{
+			y++;
+			pLine += dwInLn;
+			pPix = pLine;
+		}
+
+		while(x < wWidth)
+		{
+			bCrossCol = pXCoeff[1] > 0;
+			pUpPixOld = pUpPix;
+			pPixOld = pPix;
+			if(pXCoeff[1])
+			{
+				x++;
+				pUpPix += 3;
+				pPix += 3;
+			}
+
+			dwTmp = *pXCoeff * *pYCoeff;
+
+			for(i = 0; i < 3; i++)
+				dwPtTmp[i] = dwTmp * pUpPixOld[i];
+
+			if(bCrossCol)
+			{
+				dwTmp = pXCoeff[1] * *pYCoeff;
+				for(i = 0; i < 3; i++)
+					dwPtTmp[i] += dwTmp * pUpPix[i];
+			}
+
+			if(bCrossRow)
+			{
+				dwTmp = *pXCoeff * pYCoeff[1];
+				for(i = 0; i < 3; i++)
+					dwPtTmp[i] += dwTmp * pPixOld[i];
+				if(bCrossCol)
+				{
+					dwTmp = pXCoeff[1] * pYCoeff[1];
+					for(i = 0; i < 3; i++)
+						dwPtTmp[i] += dwTmp * pPix[i];
+				}
+			}
+
+			for(i = 0; i < 3; i++, pOutPix++)
+				*pOutPix = ((LPBYTE)(dwPtTmp + i))[3];
+
+			pXCoeff += 2;
+		}
+		pYCoeff += 2;
+	}
+
+	delete [] pRowCoeff;
+	delete [] pColCoeff;
+} 
